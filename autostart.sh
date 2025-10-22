ed2577a3eed8d1d3a2473799028b0a3700e3b7da
Christopher Daniel <cd.consulting@gmail.com>
2025-10-21 20:12:50 -0700

    changes

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# 自启动脚本 仅作参考
 
 set +e
 
@@ -10,17 +9,17 @@ dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 swaync -c ~/.config/mango/swaync/config.jsonc -s ~/.config/mango/swaync/style.css >/dev/null 2>&1 &
 
 # night light
-wlsunset -T 3501 -t 3500 >/dev/null 2>&1 &
+# wlsunset -T 5500 -t 3500 >/dev/null 2>&1 &
 
 # wallpaper
-swaybg -i ~/.config/mango/wallpaper/czd.png >/dev/null 2>&1 &
+swaybg -i -c 000000 >/dev/null 2>&1 &
 
 # top bar
 waybar -c ~/.config/mango/waybar/config.jsonc -s ~/.config/mango/waybar/style.css >/dev/null 2>&1 &
 
 
 # xwayland dpi scale
-echo "Xft.dpi: 140" | xrdb -merge #dpi缩放
+echo "Xft.dpi: 160" | xrdb -merge #dpi缩放
 # xrdb merge ~/.Xresources >/dev/null 2>&1
 
 # ime input
@@ -42,7 +41,7 @@ nm-applet >/dev/null 2>&1 &
 /usr/lib/xfce-polkit/xfce-polkit >/dev/null 2>&1 &
 
 # inhibit by audio
-sway-audio-idle-inhibit >/dev/null 2>&1 &
+# ==--sway-audio-idle-inhibit >/dev/null 2>&1 &
 
 # change light value and volume value by swayosd-client in keybind
 swayosd-server >/dev/null 2>&1 &
